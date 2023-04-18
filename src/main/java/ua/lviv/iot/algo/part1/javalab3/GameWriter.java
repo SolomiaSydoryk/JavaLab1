d9851ea3fb0972492efb509fcfe500f2c6e1a63d
SolomiaSydoryk <sydoryk213@gmail.com>
2023-04-18 23:01:40 +0300

    adding a csv file

--- src/main/java/ua/lviv/iot/algo/part1/javalab3/GameWriter.java
+++ src/main/java/ua/lviv/iot/algo/part1/javalab3/GameWriter.java
@@ -1,7 +1,29 @@
 package ua.lviv.iot.algo.part1.javalab3;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
-public class GameWritter {
-    public void writeToFile(List<Game> games){}
+public class GameWriter {
+    private static final String FILENAME = "games.csv";
+    public void writeToFile(List<Game> games) {
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter(FILENAME);
+            writer.append(games.get(0).getHeaders());
+            for (Game game : games) {
+                writer.append("\n");
+                writer.append(game.toCSV());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                writer.flush();
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
